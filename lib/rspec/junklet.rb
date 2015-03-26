2314c9261eeb157411da905c82cfc73e46ad1e0a
David Brady <dbrady@shinybit.com>
2015-03-26 14:49:59 -0600

    Add test for RSpec.configure for cases when RSpec isn't present
    * rspec-junklet autoattaches to RSpec and as such should be loaded
      from the test group only, but if you use gemspec dependencies, there
      is no test group; you can only `add_development_dependency`. So now
      we check for a working/running copy of RSpec before attempting to
      configure and autoattach.

--- lib/rspec/junklet.rb
+++ lib/rspec/junklet.rb
@@ -2,8 +2,10 @@ require_relative "./junklet/version"
 require_relative "./junklet/junk"
 require_relative "./junklet/junklet"
 
-RSpec.configure do |config|
-  config.extend(RSpec::Junklet::Junklet)
-  config.extend(RSpec::Junklet::Junk) # when metaprogramming cases, you may need junk in ExampleGroups
-  config.include(RSpec::Junklet::Junk)
+if RSpec.respond_to?(:configure)
+  RSpec.configure do |config|
+    config.extend(RSpec::Junklet::Junklet)
+    config.extend(RSpec::Junklet::Junk) # when metaprogramming cases, you may need junk in ExampleGroups
+    config.include(RSpec::Junklet::Junk)
+  end
 end
